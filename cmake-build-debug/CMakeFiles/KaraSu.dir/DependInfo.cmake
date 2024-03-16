6c4d965799365dda07608f29fcecb4760acb704b
Brando-code <brandotnt@gmail.com>
2024-03-16 17:03:19 +0100

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/KaraSu.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/brando/CLionProjects/KaraSu/IO_data.cpp" "/home/brando/CLionProjects/KaraSu/cmake-build-debug/CMakeFiles/KaraSu.dir/IO_data.cpp.o"
+  "/home/brando/CLionProjects/KaraSu/coloring.cpp" "/home/brando/CLionProjects/KaraSu/cmake-build-debug/CMakeFiles/KaraSu.dir/coloring.cpp.o"
+  "/home/brando/CLionProjects/KaraSu/graph.cpp" "/home/brando/CLionProjects/KaraSu/cmake-build-debug/CMakeFiles/KaraSu.dir/graph.cpp.o"
+  "/home/brando/CLionProjects/KaraSu/main.cpp" "/home/brando/CLionProjects/KaraSu/cmake-build-debug/CMakeFiles/KaraSu.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/KaraSu.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KaraSu.dir/IO_data.cpp.o"
+  "CMakeFiles/KaraSu.dir/coloring.cpp.o"
+  "CMakeFiles/KaraSu.dir/graph.cpp.o"
+  "CMakeFiles/KaraSu.dir/main.cpp.o"
+  "KaraSu"
+  "KaraSu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KaraSu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
